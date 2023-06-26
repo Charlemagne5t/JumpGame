48998c5e27ac3aa855dcb5f130f7b4e90e5cd764
alek <wait82985@gmail.com>
2023-06-26 18:00:04 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,18 @@
 public class Solution {
     public boolean canJump(int[] nums) {
-        //TODO
-        return false;
+        int n = nums.length;
+        boolean dp[] = new boolean[nums.length];
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            if (dp[i]) {
+                for (int j = 0; j <= nums[i] && i + j < dp.length; j++) {
+                    dp[i + j] = true;
+                }
+                if (dp[n - 1]) return true;
+            } else return false;
+        }
+
+
+        return dp[n - 1];
     }
 }
